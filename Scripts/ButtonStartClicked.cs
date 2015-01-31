758916c2e9d868d9ea9b00fe0bfeddb817dfa4d3
Cloud <eucylin@gmail.com>
2015-01-31 11:15:11 +0800

    開發出有飛入飛出的NGUI動畫 整理資料夾、刪除測試功能用的臨時project

---
+++ Scripts/ButtonStartClicked.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonStartClicked : MonoBehaviour {
+
+	public GameObject inActive;
+	public GameObject active;
+
+	void OnClick(){
+		inActive.SetActive(true);
+		active.SetActive(false);
+	}
+}
