5c474897866ec7cf9a5b3104f500582693f4dc0a
Cloud <eucylin@gmail.com>
2015-02-01 05:19:17 +0800

    增加開場、改為4個已建好的隨機序列取代完全隨機題目、增加結束場警

--- GC.cs
+++ GC.cs
@@ -8,11 +8,16 @@ public class GC : MonoBehaviour {
 	public int[] randoms;
 	public int number;
 	int tmp;
-	public void G_Random(){
-		/*randoms[0] = 3;
+	void Update(){
+		if(number > 7){
+			Application.LoadLevel(1);
+		}
+	}
+	/*public void G_Random(){
+		randoms[0] = 3;
 		randoms[1] = 1;
 		randoms[2] = 3;
-		randoms[3] = 2;*/
+		randoms[3] = 2;
 		for(int i = 0;i < randoms.Length; i++){
 			tmp = Random.Range(MIN,MAX);
 			if(i > 0 && randoms[i-1] == tmp){
@@ -26,10 +31,62 @@ public class GC : MonoBehaviour {
 			}
 			Debug.Log (randoms[i]);
 			}
-		}
+		}*/
 	void Start(){
 		randoms = new int[8];
 		number = 1;
-		G_Random();
+		tmp = Random.Range(1, 5);
+		switch(tmp)
+		{
+			case 1:
+				R1 ();
+				break;
+			case 2:
+				R2 ();
+				break;
+			case 3:
+				R3 ();
+				break;
+			default:
+				R4 ();
+				break;
+		}
+		//G_Random();
+	}
+	void R1(){
+		randoms[0] = 13;
+		randoms[1] = 1;
+		randoms[2] = 12;
+		randoms[3] = 10;
+		randoms[4] = 3;
+		randoms[5] = 2;
+		randoms[6] = 8;
+	}
+	void R2(){
+		randoms[0] = 8;
+		randoms[1] = 9;
+		randoms[2] = 6;
+		randoms[3] = 12;
+		randoms[4] = 7;
+		randoms[5] = 11;
+		randoms[6] = 14;
+	}
+	void R3(){
+		randoms[0] = 15;
+		randoms[1] = 8;
+		randoms[2] = 9;
+		randoms[3] = 7;
+		randoms[4] = 13;
+		randoms[5] = 6;
+		randoms[6] = 4;
+	}
+	void R4(){
+		randoms[0] = 3;
+		randoms[1] = 5;
+		randoms[2] = 10;
+		randoms[3] = 11;
+		randoms[4] = 13;
+		randoms[5] = 14;
+		randoms[6] = 15;
 	}
 }
