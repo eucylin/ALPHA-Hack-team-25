eb67b98b534142b02d36964ff38eb3c4fb841817
Cloud <eucylin@gmail.com>
2015-02-01 01:24:55 +0800

    SUPERRRR BIGGG PROGRESSSSSS(Random Question + Animation)

---
+++ GC.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GC : MonoBehaviour {
+	
+	public int[] randoms;
+	public void G_Random(){
+		for(int i = 0;i < randoms.Length; i++){
+			randoms[i] = Random.Range(1,3);
+			}
+		}
+	void Start(){
+		randoms = new int[4];
+		G_Random();
+	}
+}
--- Scripts/ButtonStartClicked.cs
+++ Scripts/ButtonStartClicked.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class ButtonStartClicked : MonoBehaviour {
 
-	public GameObject inActive;
+	//public GameObject inActive;
 	public GameObject active;
-
+	GC gc;
 	void OnClick(){
-		inActive.SetActive(true);
+		gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GC>();
+		//inActive.SetActive(true);
+		Debug.Log ("ADOG" + gc.randoms[0]);
+		GameObject.Find ("QuestionPanel_" + gc.randoms[0]).transform.Find("Window").gameObject.SetActive(true);
+		GameObject.Find ("LeftPanel_" + gc.randoms[0]).transform.Find("Label - Title").gameObject.SetActive(true);
+		GameObject.Find ("LeftPanel_" + gc.randoms[0]).transform.Find("Picture").gameObject.SetActive(true);
 		active.SetActive(false);
 	}
 }
