a13c0699ab4619e1babe6f3b2dd367133f26839b
Cloud <eucylin@gmail.com>
2015-01-31 16:31:44 +0800

    Healthy 3 Layouts' questions, with javascript call

---
+++ Scripts/CallJavascript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallJavascript : MonoBehaviour {
+
+	void OnClick(){
+		CallJs ();
+	}
+
+	void CallJs(){
+		Application.ExternalCall( "SwitchPage", "7" );
+	}
+}
