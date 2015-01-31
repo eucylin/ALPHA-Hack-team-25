1879d1d28f1995641ce500477623726f3beb3206
Cloud <eucylin@gmail.com>
2015-01-31 09:39:09 +0800

    First Commit

---
+++ CallJava.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallJava : MonoBehaviour {
+	Done_GameController d;
+	// Use this for initialization
+	void Start () {
+		d = GameObject.FindGameObjectWithTag("GameController").GetComponent<Done_GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(d.score >= 20f){
+			Application.ExternalCall( "SayHello", "The game says hello!" );
+			Destroy(this);
+		}
+	
+	}
+}
